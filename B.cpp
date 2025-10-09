0b0497ed312842fbbccd082e9eb9dce5ecde6712
L3isAlvaran <aach.temporary@gmail.com>
2025-10-09 21:27:53 +0700

    init commit

--- B.cpp
+++ B.cpp
@@ -1,5 +1,10 @@
 //
-// Created by aach on 08.10.2025.
-//
+// Created by aach on 08.10.2025
+#include <iostream>
 
-#include "B.h"
+int main(){
+    int a = 2;
+    int b = 5;
+
+    std::cout << a + b << std::endl;
+}
