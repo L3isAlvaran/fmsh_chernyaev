8693473ed1f8b30804099bbda50fbbce9e8eed72
L3isAlvaran <aach.temporary@gmail.com>
2025-10-15 11:03:12 +0700

    -

--- main.cpp
+++ main.cpp
@@ -13,12 +13,24 @@ string encrypt(int kluch, string slovo){
     return slovo;
 }
 
+string decrypt(int kluch, string slovo) {
+    for (int i = 0; i <= slovo.size(); i = i + 1) {
+        if (slovo[i] - kluch >= 'a' and slovo[i] <= 'z') {
+            slovo[i] = slovo[i] - kluch;
+        } else if (slovo[i] - kluch < 'a' and slovo[i] <= 'z') {
+            slovo[i] = slovo[i] - kluch + 26;
+        }
+    }
+    return slovo;
+}
+
 int main() {
     int n = 0;
     std::cin >> n;
-    n = n % 26;
+    n = n % 26; //на случай если смещение больше чем букв в алфавите (например если смещение равно 29, то это то же самое что смещение на 3)
     string s = "hello";
     cin >> s;
     std::cout << encrypt(n, s) << std::endl;
+    std::cout << decrypt(n, s) << std::endl;
     return 0;
 }
