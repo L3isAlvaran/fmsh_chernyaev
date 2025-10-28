4b2c07f3798d016ba3e5fb96b3f7610ef9ced039
L3isAlvaran <aach.temporary@gmail.com>
2025-10-28 23:15:19 +0700

    B1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <string>
 #include <iostream>
+#include <funkciya.h>
+using namespace std;
 
 int main() {
-
+    string data = "СЛОВО";
+    string key = "КЛЮЧИ";
+    funcB(data, key);
 }
